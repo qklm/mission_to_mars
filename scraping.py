4510465b66f9fbee7dbffea74dcbfb922463127a
David Brown <quinsoq@gmail.com>
2021-05-10 08:17:14 -0700

    scraping all function integrated

--- scraping.py
+++ scraping.py
@@ -3,10 +3,24 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
+def scrape_all():
 #set up splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    browser.quit()
+    return data
+
 
 def mars_news(browser):
 # visit mars news site
