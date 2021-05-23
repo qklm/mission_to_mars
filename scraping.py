d50e46ca579f5fc754ad77411597d360f0faab95
David Brown <quinsoq@gmail.com>
2021-05-23 09:31:43 -0700

    small adjustments

--- scraping.py
+++ scraping.py
@@ -7,8 +7,8 @@ import datetime as dt
 
 def scrape_all():
 #set up splinter
-    executable_path = {'executable_path': "chromedriver"}
-    browser = Browser('chrome', **executable_path, headless=False)
+    executable_path = {'executable_path': ChromeDriverManager.install()}
+    browser = Browser('chrome', **executable_path, headless=True)
     news_title, news_paragraph = mars_news(browser)
     data = {
         "news_title": news_title,
@@ -24,7 +24,7 @@ def scrape_all():
 
 def mars_news(browser):
 # visit mars news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    url = 'https://redplanetscience.com'
     browser.visit(url)
     browser.is_element_not_present_by_css('div.list_text', wait_time=1)
 
@@ -43,7 +43,7 @@ def mars_news(browser):
 # ## Featured Images
 def featured_image(browser):
     # visit url
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     full_image_elem = browser.find_by_tag('button')[1]
@@ -55,15 +55,14 @@ def featured_image(browser):
         img_url_rel = img_soup.find('img', class_ = 'fancybox-image').get('src')
     except AttributeError:
         return None
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
     return img_url
 
 def mars_facts():
     try:
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
     except BaseException:
         return None
-
     df.columns = ['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
     return df.to_html(classes="table table-striped")
